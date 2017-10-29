1fca115617e16f0fb5832e714e863129465b0d7d
Eduardo Medina <emedinaa@gmail.com>
2017-10-28 23:04:04 -0500

    Add sample

---
+++ QMC/cmake-build-debug/CMakeFiles/QMC.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/emedinaa/Documents/UIGV/MatematicaDiscreta/Repo/cpp-quine-mccluskey-algorithm/QMC/main.cpp" "/Users/emedinaa/Documents/UIGV/MatematicaDiscreta/Repo/cpp-quine-mccluskey-algorithm/QMC/cmake-build-debug/CMakeFiles/QMC.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ QMC/cmake-build-debug/CMakeFiles/QMC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QMC.dir/main.cpp.o"
+  "QMC.pdb"
+  "QMC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QMC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
